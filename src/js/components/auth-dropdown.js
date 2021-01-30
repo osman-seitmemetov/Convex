b0e17eb41a26af8d0daaea273e6f060d8cc7b78a
Osman <69184289+Verstalshik-na-zakaz@users.noreply.github.com>
2021-01-30 16:35:32 +0300

    Delete src directory

--- src/js/components/auth-dropdown.js
+++
@@ -1,8 +0,0 @@
-const headerAuth = document.querySelector('.header__auth');
-const headerAuthBtn = document.querySelector('.header__auth-btn');
-
-if (headerAuthBtn) {
-    headerAuthBtn.addEventListener('click', () => {
-        headerAuth.classList.toggle('header__auth--active');
-    });
-}
