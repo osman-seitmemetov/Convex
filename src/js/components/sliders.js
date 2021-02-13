e082f3309cf0b60b6c8752aa19dd6fa099f57d2a
Osman <69184289+Osman228@users.noreply.github.com>
2021-02-13 15:38:20 +0300

    Адаптировал главную страницу

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -1,14 +1,14 @@
 import Swiper from '../vendor/swiper.min.js';
 
-// let swiper = new Swiper('.banner__slider', {
-//     slidesPerView: 1,
-//     loop: true,
-//     autoplay: true,
-//     navigation: {
-//       nextEl: '.banner__button-next',
-//       prevEl: '.banner__button-prev',
-//     },
-// });
+let bannerSlider = new Swiper('.banner__slider', {
+    slidesPerView: 1,
+    loop: true,
+    autoplay: true,
+    navigation: {
+        nextEl: '.banner__button-next',
+        prevEl: '.banner__button-prev',
+    },
+});
 
 // let products = new Swiper('.products__slider', {
 //     slidesPerView: 4,
@@ -27,18 +27,54 @@ productSlider.forEach(el => {
             nextEl: '.products__button-next',
             prevEl: '.products__button-prev',
         },
+        IOSEdgeSwipeDetection: true,
         breakpoints: {
             320: {
-                slidesPerView: 2,
+                slidesPerView: 'auto',
                 freeMode: true,
+                spaceBetween: 15,
+                watchOverflow: false
+            },
+
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+                watchOverflow: true,
+                freeMode: false,
             },
 
             992: {
                 slidesPerView: 4,
+                spaceBetween: 20,
+                watchOverflow: true,
+                freeMode: false,
+            },
+
+            1440: {
+                slidesPerView: 4,
                 spaceBetween: 60,
+                watchOverflow: true,
+                freeMode: false,
             }
         }
     });
+});
+
+let proposSlider = new Swiper('.propos__slider', {
+    IOSEdgeSwipeDetection: true,
+    breakpoints: {
+        320: {
+            slidesPerView: 'auto',
+            freeMode: true,
+            spaceBetween: 15
+        },
+
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 15,
+            watchOverflow: true
+        }
+    }
 });
 
 let selectSwiper = new Swiper('.select__body', {
